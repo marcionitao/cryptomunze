f1a3b8992ed8e05951f226f73a661763d350416f
Marcio Nitao <marcionitao@gmail.com>
2022-08-15 10:09:18 +0200

    add NProgress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,28 @@ import 'tailwindcss/tailwind.css';
 import { ApiProvider } from '../data/context/ApiContext';
 import '../styles/globals.css';
 
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import '../styles/nprogress.css';
+
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+NProgress.configure({
+  showSpinner: false,
+  trickleSpeed: 200,
+});
+
 function MyApp({ Component, pageProps }) {
+  // quando mudar de rota, o nprogress é iniciado
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => NProgress.start());
+    router.events.on('routeChangeComplete', () => NProgress.done());
+    router.events.on('routeChangeError', () => NProgress.done());
+  }, []);
+  //
   return (
     // neste caso, 'ApiProvider' está provendo dados p/ todos os componentes da aplicação
     <ApiProvider>
