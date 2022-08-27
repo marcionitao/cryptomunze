31a2640bb51f3645397f3716af65e2ac477dbf61
Marcio Nitao <marcionitao@gmail.com>
2022-08-27 23:40:22 +0200

    configure Link in Footer acess

--- src/components/template/Footer.tsx
+++ src/components/template/Footer.tsx
@@ -1,33 +1,33 @@
 import moment from 'moment';
+import Link from 'next/link';
 export default function Footer() {
   //
   const ano = moment().year();
-
+  //
   return (
     <>
       <footer className="p-1 mt-2 bg-gray-400 shadow sm:flex sm:items-center sm:justify-between sm:p-2 bg-opacity-10">
         <span className="flex justify-center text-xs text-gray-400 sm:text-center">
           {`© ${ano}`}&nbsp;
-          <a href="https://flowbite.com/" className="hover:underline">
-            CrypoMunze
-          </a>
-          . All Rights Reserved.
+          <Link href="/">
+            <p className="cursor-pointer">CrypoMunze. All Rights Reserved.</p>
+          </Link>
         </span>
         <ul className="flex flex-wrap items-center justify-center text-xs text-gray-400">
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6 ">
-              About
-            </a>
+            <Link href="/about">
+              <p className="mr-4 md:mr-6 cursor-pointer">About</p>
+            </Link>
           </li>
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">
-              Privacy Policy
-            </a>
+            <Link href="/list">
+              <p className="mr-4 md:mr-6 cursor-pointer">Coin List</p>
+            </Link>
           </li>
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">
-              Licensing
-            </a>
+            <Link href="#">
+              <p className="mr-4 md:mr-6 cursor-pointer">Privacy Policy</p>
+            </Link>
           </li>
         </ul>
       </footer>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,23 +1,32 @@
 import Link from 'next/link';
+import Footer from '../components/template/Footer';
 import Logo from '../components/template/Logo';
 
 export default function About() {
   return (
-    <div className="flex justify-center p-14">
-      <div className="p-6 max-w-md  rounded-lg border shadow-md bg-opacity-10 bg-gray-400 border-gray-700">
-        <Logo titulo="CrypoMunze" src="/images/logo.png" />
-        <a href="#">
-          <h5 className="mb-2 mt-4 text-2xl font-semibold tracking-tight text-white">
-            Need a help in Claim?
-          </h5>
-        </a>
-        <p className="mb-3 font-normal text-gray-400">
-          Go to this step by step guideline process on how to certify for your weekly benefits:
-        </p>
-        <Link href={'/'}>
-          <p className="text-yellow-300 cursor-pointer">Back to home</p>
-        </Link>
+    <div className={`flex h-screen w-auto flex-col`}>
+      <div className="flex justify-center p-14 flex-1">
+        <div className="p-6 max-w-md  rounded-lg border shadow-md bg-opacity-10 bg-gray-400 border-gray-700">
+          <Logo titulo="CrypoMunze" src="/images/logo.png" />
+          <a href="#">
+            <h5 className="mb-2 mt-4 text-2xl font-semibold tracking-tight text-white">About us</h5>
+          </a>
+          <p className="mb-3 font-normal text-gray-400">
+            Cryptocurrencies emerged in 2009 with the first decentralized cryptocurrency - Bitcoin .
+            As of today - more than 700 digital currencies exist with a total market capitalization
+            greater than 100 billion USD.
+          </p>
+          <p className="mb-3 font-normal text-gray-400">
+            The CryptoMünze is a real-time dashboard that displays the top 10 cryptocurrencies based
+            on currency price, market capitalization and overall circulating supply - obtained from
+            the leading cryptocurrency resource CryptoCompare.
+          </p>
+          <Link href={'/'}>
+            <p className="text-yellow-300 cursor-pointer">Back to home</p>
+          </Link>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
